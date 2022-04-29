0574b3a14186589ff30736ddebc70288d863050f
Duy <104508574+nnduy289@users.noreply.github.com>
2022-04-29 22:50:35 +0700

    chapter 2 video 3

---
+++ video3.js
@@ -0,0 +1,10 @@
+// const x = 5;
+
+// x = 6;
+    
+console.log(`Hello "'""""'''"  world form javascript: `)
+
+let x = 5, y = 6, z = 7;
+
+// console.log("Test string", 'x= ', x, 'y= ', y, 'z= ', z)
+console.log("Test string 2222", + 'x= ' + x + 'y= ' + y + 'z= ' + z)
